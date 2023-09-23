13212640550205581a9749a5a99fbb44e02dae7c
l-Spook-l <uaspookua@gmail.com>
2023-09-23 23:24:27 +0300

    changed and configured index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import UserStore from './stores/UserStore'
+import WorkoutStore from './stores/WorkoutStore';
+
+
+// Создаем глобальную перевенную, это будет обьект, 
+// который будет доступен компонентам на все уровнях вложенности
+// с помощью хука useContext
+// В любом компоненте можно их вызвать так
+// const {user} = useContext(Context)
+// const {product} = useContext(Context)
+export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Context.Provider
+  // value - обьект который нам нужно сделать глобальным
+  // их и вызываем с помощью хука useContext
+    value={{
+      //user: new UserStore(),
+      workout: new WorkoutStore(),
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Context.Provider>
+)
