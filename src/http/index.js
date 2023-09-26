7bf79a988ffd1c958957e64e5ee161e6bfc5dc85
l-Spook-l <uaspookua@gmail.com>
2023-09-26 23:32:08 +0300

    create connect file

---
+++ src/http/index.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+//console.log(process.env.REACT_APP_API_URL)
+const $host = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const $authHost = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const authInterceptor = (config) => {
+  config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
+/*   console.log('config.headers.authorization', config.headers.authorization)
+  console.log('config.headers', config.headers)
+  console.log('config', config) */
+  return config;
+};
+
+$authHost.interceptors.request.use(authInterceptor);
+
+export { $host, $authHost };
