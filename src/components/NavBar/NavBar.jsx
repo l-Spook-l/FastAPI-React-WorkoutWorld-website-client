eadaef80bef6b4862b166380d0bbec86ef47e1b2
l-Spook-l <uaspookua@gmail.com>
2023-09-27 16:30:35 +0300

    Improved styles for the navbar.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,9 +2,10 @@ import { observer } from 'mobx-react-lite'
 import React, { useContext, useState } from 'react'
 import { Context } from '../..'
 import { NavLink, useNavigate } from 'react-router-dom'
-import { Container, Navbar } from 'react-bootstrap'
-import { MAIN_ROUTE } from '../../utils/consts'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { MAIN_ROUTE, PROFILE_ROUTE, WORKOUTS_ROUTE } from '../../utils/consts'
 import style from "./NavBar.module.css";
+import { AiOutlineProfile, AiOutlineLogin, AiOutlineLogout } from "react-icons/ai";
 
 const NavBar = observer(() => {
   const { user } = useContext(Context)
@@ -14,22 +15,87 @@ const NavBar = observer(() => {
 
   const [showLogin, setShowLogin] = useState(true);
   const [showModal, setShowModal] = useState(false);
+  
+  // Выход из профиля
+  const logOut = () => {
+    user.setUser({});
+    user.setIsAuth(false);
+    localStorage.setItem("token", null);
+  };
+
+  const clickLogin = () => {
+    //setShowModal(true);
+    //setShowLogin(true);
+    user.setIsAuth(true)  // для теста
+  };
 
   return (
-    <Container className={style.navbarContainer}>
       <Navbar 
         collapseOnSelect
-        expand="lg"
-        bg="black"
-        variant="dark"
-        className="fixed-top"
-        style={{ height: "63px" }}>
-        <NavLink className={style.logoContainer} to={MAIN_ROUTE}>
-          <span className={style.logoText}>WorkoutWorld</span>
+        //expand="lg"
+        //bg="black"
+        //variant="dark"
+        className={style.myNavbar}
+        >
+        <NavLink className={style.logo} to={MAIN_ROUTE}>
+          WorkoutWorld
         </NavLink>
+        <Nav className={style.menuNavbar}>
+          <NavLink className={style.workouts} to={WORKOUTS_ROUTE}>
+            Workouts
+          </NavLink>
+          <div>
+            {user.isAuth ? 
+              <div>
+                <NavLink
+                  className={style.profile}
+                  to={{ pathname: PROFILE_ROUTE }}
+                  // + state="userInfo"
+                >
+                  <AiOutlineProfile />
+                </NavLink>
+                <NavLink className={style.login} onClick={logOut} to={MAIN_ROUTE}>
+                  <AiOutlineLogout />
+                </NavLink>
+              </div>
+             : 
+              <NavLink className={style.logout} onClick={clickLogin}>
+                <AiOutlineLogin />
+              </NavLink>
+            }
+          </div>
+        </Nav>
+        {/* <div className={style.menuNavbar}>
+          <NavLink to={WORKOUTS_ROUTE}>
+            Workouts
+          </NavLink>
+          <Nav>wishlist</Nav>
+          <Nav>Profile</Nav>
+        </div> */}
+        {/* <div className="d-flex justify-content-center">
+          <Nav>
+            {user.isAuth ? 
+              <div>
+                <NavLink
+                  className={style.profile}
+                  to={{ pathname: PROFILE_ROUTE }}+
+                  state="userInfo"
+                >
+                  <AiOutlineProfile />
+                </NavLink>
+                <NavLink className={style.login} onClick={logOut} to={MAIN_ROUTE}>
+                  <AiOutlineLogout />
+                </NavLink>
+              </div>
+             : 
+              <NavLink className={style.logout} onClick={clickLogin}>
+                <AiOutlineLogin />
+              </NavLink>
+            }
+            </Nav>
+        </div> */}
       </Navbar>
       
-    </Container>
   )
 })
 
