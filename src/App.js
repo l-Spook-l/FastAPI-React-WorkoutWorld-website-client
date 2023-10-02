9e310d7c994f4414320a27d141743e44c9600747
l-Spook-l <uaspookua@gmail.com>
2023-10-02 23:57:41 +0300

    added the function to check the user token

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import { observer } from 'mobx-react-lite'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './components/AppRouter'
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
+import { Context } from '.'
+import { check } from './http/userAPI'
+import { Spinner } from 'react-bootstrap'
 
 
 const App = observer(() => {
+  const { user } = useContext(Context)
+  const { workout } = useContext(Context)
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    console.log('начал работать useEffect check', user)
+
+    check().then((data) => {
+      console.log('qqqqqqqqqqqqqqq', data)
+      user.setUser(data)
+      user.setIsAuth(true)
+    }).catch((error) => {
+      console.log('Error login', error);
+      // Обработка других ошибок, возникших при выполнении запроса
+    }).finally(() => setLoading(false))
+
+  }, [])
+
+  if (loading) {
+    return <Spinner animation='grow'/>
+  }
+
   return (
     <BrowserRouter>
       <NavBar/>
-      <AppRouter/>
+      <div style={{ marginTop: "63px" }}>
+        <AppRouter />
+      </div>
       <Footer/>
     </BrowserRouter>
   )
