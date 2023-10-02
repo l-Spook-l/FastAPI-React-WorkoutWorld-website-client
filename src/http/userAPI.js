2ae341d19c541221fa7003f9e94962bb4ef7637c
l-Spook-l <uaspookua@gmail.com>
2023-10-02 23:58:22 +0300

    added the function to check the user token

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -2,30 +2,38 @@ import jwt_decode from "jwt-decode";
 import { $authHost, $host } from "./index";
 //import { createBasketForUser, createWishListForUser } from "./productAPI";
 
-export const registration = async (first_name, last_name, email, password) => {
-  console.log('registration', first_name, last_name, email, password)
-  const response = await $host.post("auth/users/", { first_name, last_name, email, password });  // Ответ от сервера
-  console.log("регистрация response", response);
+export const registration = async (first_name, last_name, email, password, username, phone=0) => {
+  //console.log('registration', first_name, last_name, email, password, username)
+  const response = await $host.post("auth/register/", { email, password, username, first_name, last_name, phone });  // Ответ от сервера
+  //console.log("регистрация response", response);
   //createBasketForUser({user: response.data.id})  // создаем корзину
   //createWishListForUser({user: response.data.id})  // создаем список желаний 
-  console.log("регистрация data", response.data);
   return response.data
 };
 
-export const login = async (email, password) => {
-  const response = await $host.post("auth/jwt/create/", { email, password });
+export const login = async ( username, password ) => {
+  console.log('login', username, password)
+  const response = await $host.post("auth/jwt/login/", 
+    { username, password }, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
   console.log("авторизация response", response); 
   console.log("авторизация data", response.data);
-  localStorage.setItem("token", response.data.access); // глянь на этот ответ 2:11:7
-  console.log("авторизация response.data.access", response.data.access); 
-  console.log("авторизация jwt_decode(response.data.access)", jwt_decode(response.data.access)); 
+  localStorage.setItem("token", response.data.access_token); // глянь на этот ответ 2:11:7
+  console.log("авторизация response.data.access_token", response.data.access_token); 
+  console.log("авторизация jwt_decode(response.data.access_token)", jwt_decode(response.data.access_token)); 
+  console.log("авторизация jwt_decode(response.data.access_token)", jwt_decode(response.data.access_token).sub); 
 
-  const userId = jwt_decode(localStorage.getItem("token")).user_id
-  const responseUserMe = await $authHost.get(`api/v1/user-info/${userId}/`);
+  // const userId = jwt_decode(localStorage.getItem("token")).sub  protected-route
+  // const responseUserMe = await $authHost.get(`api/v1/user-info/${userId}/`);
+  const responseUserMe = await $authHost.get(`protected-route`);
   console.log("авторизация auth/users/me/ responseUserMe", responseUserMe);
   console.log("авторизация auth/users/me/ responseUserMe data", responseUserMe.data);
 
-  return responseUserMe.data.results[0];
+  //return responseUserMe.data.results[0];
+  return response;
 };
 
 // Проверка токена на валидность
@@ -37,11 +45,13 @@ export const check = async () => {
   console.log("check in userAPI is work token")
 
   const userId = jwt_decode(localStorage.getItem("token")).user_id
-  const response = await $authHost.get(`api/v1/user-info/${userId}/`);
+  //const response = await $authHost.get(`api/v1/user-info/${userId}/`);
+  const response = await $authHost.get(`protected-route`);
 
   console.log("check auth/users/me/ token test", `Bearer ${localStorage.getItem("token")}`);
   console.log("check auth/users/me/ response user-info", response);
   console.log("check auth/users/me/ response.data user-info", response.data);
 
-  return response.data.results[0]
+  //return response.data.results[0]
+  return response.data
 };
