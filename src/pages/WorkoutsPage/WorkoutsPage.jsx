6803305cb5ceb35adf4d7673a210ba4ace0d564e
l-Spook-l <uaspookua@gmail.com>
2023-10-04 16:51:07 +0300

    Now only authorized users can create a workout

--- src/pages/WorkoutsPage/WorkoutsPage.jsx
+++ src/pages/WorkoutsPage/WorkoutsPage.jsx
@@ -1,10 +1,13 @@
 import { observer } from 'mobx-react-lite'
 import React, { useContext, useEffect, useState } from 'react'
-import { Container, Spinner } from 'react-bootstrap'
+import { Breadcrumb, Container, Spinner } from 'react-bootstrap'
 import { Context } from '../..'
 import { fetchWorkouts } from '../../http/workoutAPI'
 import WorkoutList from '../../components/WorkoutList/WorkoutList'
-import { useParams } from 'react-router-dom'
+import { NavLink, useNavigate, useParams } from 'react-router-dom'
+import { CREATE_WORKOUT_ROUTE, MAIN_ROUTE } from '../../utils/consts'
+import style from './WorkoutsPage.module.css'
+import MyPagination from '../../components/MyPagination/MyPagination'
 
 
 const WorkoutsPage = observer(() => {
@@ -12,26 +15,34 @@ const WorkoutsPage = observer(() => {
   const { workout } = useContext(Context)
   const { slug } = useParams()
 
+  const navigate = useNavigate();
+
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    //window.scrollTo(0, 0)
+    window.scrollTo(0, 0)
     fetchWorkouts().then((data) => {
       workout.setWorkouts(data)
     }).finally(() => setLoading(false))
   }, [])
-
+  
   if (loading) {
     return <Spinner animation='grow'/>
   }
 
   return (
-    <Container>
-      {/* {console.log('ewew', workout)}
-      {console.log('ewew2', workout.workouts)}
-      {console.log('ewew3', workout.workouts.data)} */}
-      WorkoutsPage
-      <WorkoutList/>
+    <Container fluid className={style.container}>
+      <Breadcrumb className="mt-2">
+        <Breadcrumb.Item onClick={() => navigate(MAIN_ROUTE)}>
+          Home
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>Workouts</Breadcrumb.Item>
+      </Breadcrumb>
+      {user.isAuth && <NavLink to={CREATE_WORKOUT_ROUTE}>Create workout</NavLink> }
+      <div className={style.workoutSections}>
+        <WorkoutList/>
+        <MyPagination/>
+      </div>
     </Container>
   )
 })
