360df2f545b3379f6814063c449725105a49eb67
l-Spook-l <uaspookua@gmail.com>
2023-10-05 15:40:40 +0300

    added the capability to create workouts, exercises, and sets

--- src/http/workoutAPI.js
+++ src/http/workoutAPI.js
@@ -18,3 +18,22 @@ export const fetchSets = async(exercise, user) => {
   console.log('fetchSets response', response)
   return response.data
 }
+
+// изменить host - на auhost
+export const createWorkout = async(name, user_id, description, difficulty) => {
+  const response = await $host.post(`workouts/create_workout/`, {name, user_id, description, difficulty})
+  console.log('fetchSets response', response)
+  return response.data
+}
+
+export const createExercise = async(workout_id, description, number_of_sets, maximum_repetitions, rest_time, weight, timer) => {
+  const response = await $host.post(`workouts/create_exercise/`, {workout_id, description, number_of_sets, maximum_repetitions, rest_time, weight, timer})
+  console.log('fetchSets response', response)
+  return response.data
+}
+
+export const createSet = async(exercise_id, user_id, count) => {
+  const response = await $host.post(`workouts/create_set/`, {exercise_id, user_id, count})
+  console.log('fetchSets response', response)
+  return response.data
+}
