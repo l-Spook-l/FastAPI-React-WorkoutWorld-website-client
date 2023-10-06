b9ed85b8c92f79caa796d42feb6acde9b216bd9c
l-Spook-l <uaspookua@gmail.com>
2023-10-06 16:35:13 +0300

    added the 'skip' parameter to the fetchWorkouts function

--- src/http/workoutAPI.js
+++ src/http/workoutAPI.js
@@ -1,8 +1,8 @@
 import { $host } from "./index"
 
 
-export const fetchWorkouts = async() => {
-  const response = await $host.get('workouts/')
+export const fetchWorkouts = async(skip) => {
+  const response = await $host.get(`workouts/`, {params: {skip:skip}})
   console.log('fetchWorkouts response', response)
   return response.data
 }
@@ -20,21 +20,20 @@ export const fetchSets = async(exercise, user) => {
 }
 
 // изменить host - на auhost
-export const createWorkout = async(name, user_id, description, difficulty, total_time) => {
+export const createWorkout = async(name, user_id, description, difficulty='test', total_time) => {
   const response = await $host.post(`workouts/create_workout/`, {name, user_id, description, difficulty, total_time})
   console.log('createWorkout response', response)
   return response.data
 }
 
-export const createExercise = async(workout_id, description, number_of_sets, maximum_repetitions, rest_time, video, photo) => {
-  const response = await $host.post(`workouts/create_exercise/`, {workout_id, description, number_of_sets, maximum_repetitions, rest_time, video, photo})
+export const createExercise = async(name, workout_id, description, number_of_sets, maximum_repetitions, rest_time, video, photo) => {
+  const response = await $host.post(`workouts/create_exercise/`, {name, workout_id, description, number_of_sets, maximum_repetitions, rest_time, video, photo})
   console.log('createExercise response', response)
   return response.data
 }
 
-export const createSet = async(exercise_id, user_id, count) => {
+export const createSet = async(exercise_id, user_id, count, weight) => {
   const response = await $host.post(`workouts/create_set/`, {exercise_id, user_id, count, weight})
   console.log('createSet response', response)
   return response.data
 }
-
