384215e0b2e5102c86dcb4a3b85256887cb9e2f4
l-Spook-l <uaspookua@gmail.com>
2023-10-09 22:24:41 +0300

    A button for editing a workout has been created, accessible if the workout was created by the user

--- src/components/ExerciseItem/ExerciseItem.jsx
+++ src/components/ExerciseItem/ExerciseItem.jsx
@@ -1,16 +1,36 @@
 import { observer } from 'mobx-react-lite'
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import React, { useContext, useState } from 'react'
+import { Card, Col, Row } from 'react-bootstrap'
+import { Context } from '../..'
+import { AiFillEdit} from "react-icons/ai";
+import style from './ExerciseItem.module.css'
 
 const ExerciseItem = observer(
-  ({ exerciseId, description, numberOfSets, maximumRepetitions, restTime, weight, timer }) => {
+  ({ name, description, numberOfSets, maximumRepetitions, restTime }) => {
+  
+  const { user } = useContext(Context)
+  const { workout } = useContext(Context)
+
+  const [editWorkout, setEditWorkout] = useState(false)
+  
   return (
-    <Row>
-      ExerciseItem
-      <Col md={4}>{numberOfSets}</Col>
-      <Col md={4}>{maximumRepetitions}</Col>
-      <Col md={4}>{restTime}</Col>
-    </Row>
+    <Card>
+      <Card.Body>
+        <div className={style.titleSection}>
+          <h3>{name}</h3>
+          {(user.user.id === workout.selectedWorkout.data.Workout.user_id) && 
+            <button onClick={() => setEditWorkout(true)}><AiFillEdit/></button>
+          }
+        </div>
+        <p>description: {description}</p>
+        
+        <Row>
+          <Col md={4}>numberOfSets: {numberOfSets}</Col>
+          <Col md={4}>maximumRepetitions: {maximumRepetitions}</Col>
+          <Col md={4}>restTime: {restTime}</Col>
+        </Row>
+      </Card.Body>
+    </Card>
   )
 })
 
