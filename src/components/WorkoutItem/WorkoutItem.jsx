9525522056b3d749068d33aa865fe6dbeafeb13c
l-Spook-l <uaspookua@gmail.com>
2023-10-09 21:44:54 +0300

    A button for editing a workout has been created, accessible if the workout was created by the user

--- src/components/WorkoutItem/WorkoutItem.jsx
+++ src/components/WorkoutItem/WorkoutItem.jsx
@@ -1,8 +1,12 @@
 import { observer } from 'mobx-react-lite'
 import React, { useContext, useEffect } from 'react'
 import { Card } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { Context } from '../..'
+import style from "./WorkoutItem.module.css"
+import { WORKOUT_ROUTE } from '../../utils/consts'
+import { AiFillEdit} from "react-icons/ai";
+
 
 const WorkoutItem = observer(({ workout }) => {
   const { user } = useContext(Context)
@@ -15,12 +19,26 @@ const WorkoutItem = observer(({ workout }) => {
 
   return (
     <div>
-      WorkoutItem
-      <Card>
-        <p>{workout.id}</p>
-        <p>{workout.name}</p>
-        <p>{workout.description}</p>
-        <p>{workout.difficulty}</p>
+      <Card className={style.myCard}>
+        <Card.Body>
+          <div>
+          <NavLink className={style.nameWorkout} to={`${WORKOUT_ROUTE}/${workout.id}`}>
+            {workout.name}
+          </NavLink>
+          {(user.user.id === workout.user_id) && 
+          <NavLink>
+            <AiFillEdit/>
+          </NavLink>
+          }
+          </div>
+          
+          <div>
+            Description
+            <p>{workout.description}</p>
+            <p>Difficulty {workout.difficulty}</p>
+          </div>
+          <button onClick={() => navigate(`${WORKOUT_ROUTE}/${workout.id}`)}>See more</button>
+        </Card.Body>        
       </Card>
     </div>
   )
