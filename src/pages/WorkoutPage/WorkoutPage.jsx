662f4be7b9e8fa692fe3231526cce11a0860899f
l-Spook-l <uaspookua@gmail.com>
2023-10-10 17:23:54 +0300

    Implemented updating of workout title, description, and difficulty

--- src/pages/WorkoutPage/WorkoutPage.jsx
+++ src/pages/WorkoutPage/WorkoutPage.jsx
@@ -1,14 +1,16 @@
 import { observer } from 'mobx-react-lite'
 import React, { useContext, useEffect, useState } from 'react'
 import { Context } from '../..'
-import { useNavigate, useParams } from 'react-router-dom'
-import { fetchOneWorkout } from '../../http/workoutAPI'
+import { NavLink, useNavigate, useParams } from 'react-router-dom'
+import { fetchOneWorkout, updateWorkout } from '../../http/workoutAPI'
 import { Breadcrumb, Card, Col, Container, Row, Spinner } from 'react-bootstrap'
 import { MAIN_ROUTE } from '../../utils/consts'
 import ExerciseItem from '../../components/ExerciseItem/ExerciseItem'
+import { AiFillEdit, AiOutlineCheck} from "react-icons/ai";
 import style from './WorkoutPage.module.css'
 
 const WorkoutPage = observer(() => {
+  const { user } = useContext(Context)
   const { workout } = useContext(Context)
   const { workout_id }  = useParams();
 
@@ -16,9 +18,20 @@ const WorkoutPage = observer(() => {
 
   const [loading, setLoading] = useState(true);
   
+  const [editWorkout, setEditWorkout] = useState(false)
+
+  const [workoutName, setWorkoutName] = useState('')
+  const [workoutDifficulty, setWorkoutDifficulty] = useState('')
+  const [workoutDescription, setWorkoutDescription] = useState('')
+
   useEffect(() => {
     fetchOneWorkout(workout_id)
-    .then((data) => workout.setSelectedWorkout(data))
+    .then((data) => {
+      workout.setSelectedWorkout(data)
+      setWorkoutName(data.data.Workout.name)
+      setWorkoutDifficulty(data.data.Workout.difficulty)
+      setWorkoutDescription(data.data.Workout.description)
+    })
     .finally(() => setLoading(false))
   },[workout_id])
 
@@ -26,74 +39,60 @@ const WorkoutPage = observer(() => {
     return <Spinner animation="grow" />;
   }
 
+  const updateParamWorkout = () => {
+    updateWorkout(workoutName, workout_id, workoutDescription, workoutDifficulty)
+    setEditWorkout(false)
+  }
+
   return (
-    // <Container>
-    //   <Breadcrumb>
-    //     <Breadcrumb.Item onClick={() => navigate(MAIN_ROUTE)}>
-    //         Home
-    //       </Breadcrumb.Item>
-    //   </Breadcrumb>
-    //   <hr/>
-    //   <p>name {workout.selectedWorkout.data.Workout.name}</p>
-    //   <p>description {workout.selectedWorkout.data.Workout.description}</p>
-    //   <p>difficulty {workout.selectedWorkout.data.Workout.difficulty}</p>
-    //   <p>total_time {workout.selectedWorkout.data.Workout.total_time}</p>
-    //   exercise
-    //   <div>
-    //     {workout.selectedWorkout.data.Workout.exercise.map((exercise) => 
-    //       <div key={exercise.id}>
-    //         <ExerciseItem
-    //           key={exercise.id}
-    //           exerciseId={exercise.id}
-    //           name={exercise.name}
-    //           description={exercise.description}
-    //           numberOfSets={exercise.number_of_sets}
-    //           maximumRepetitions={exercise.maximum_repetitions}
-    //           restTime={exercise.rest_time}
-    //         />
-    //       </div>
-    //     )}
-    //   </div>
-      
-    // </Container>
     <Container className={style.workoutContainer}>
-    <h1>Workout</h1>
-    <Row>
-      <Col md={8}>
-        <Card className={style.workoutCard}>
-          <Card.Body>
-            <div className={style.workoutTitle}>
-              <Card.Title>{workout.selectedWorkout.data.Workout.name}</Card.Title>
-              <button>Start workout</button>
-            </div>
-            <Card.Subtitle className="mb-2 text-muted">{workout.selectedWorkout.data.Workout.difficulty}</Card.Subtitle>
-            <Card.Text className={style.workoutCardText}>{workout.selectedWorkout.data.Workout.description}</Card.Text>
-            <Card.Text>Exercises:</Card.Text>
-            <ul className={style.exerciseList}>
-              {/* {workout.exercises.map(exercise => (
-                <li key={exercise.id} className={style.exerciseListItem}>
-                  {exercise.name} - {exercise.difficulty}
-                </li>
-              ))} */}
-              {workout.selectedWorkout.data.Workout.exercise.map((exercise) => 
-                <div key={exercise.id}>
-                  <ExerciseItem
-                    key={exercise.id}
-                    exerciseId={exercise.id}
-                    name={exercise.name}
-                    description={exercise.description}
-                    numberOfSets={exercise.number_of_sets}
-                    maximumRepetitions={exercise.maximum_repetitions}
-                    restTime={exercise.rest_time}
-                  />
-                </div>
-              )}
-            </ul>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  </Container>
+      <h1>Workout</h1>
+      <Row>
+        <Col md={8}>
+          <Card className={style.workoutCard}>
+            <Card.Body>
+              <div className={style.workoutTitle}>
+                {editWorkout
+                ? <input type="text" value={workoutName} onChange={(el) => setWorkoutName(el.target.value)} />
+                : <Card.Title>{workoutName}</Card.Title>
+                }
+                <button>Start workout</button>
+                {(user.user.id === workout.selectedWorkout.data.Workout.user_id) && 
+                  editWorkout
+                  ?  <button onClick={() => updateParamWorkout()}><AiOutlineCheck/></button>
+                  :  <button onClick={() => setEditWorkout(true)}><AiFillEdit/></button>
+                }
+              </div>
+              {editWorkout
+                ? <input type="text" value={workoutDifficulty} onChange={(el) => setWorkoutDifficulty(el.target.value)} />
+                : <Card.Subtitle className="mb-2 text-muted">difficulty: {workoutDifficulty}</Card.Subtitle>
+              }
+              <p>description: </p>
+              {editWorkout
+                ? <input type="text" value={workoutDescription} onChange={(el) => setWorkoutDescription(el.target.value)} />
+                : <Card.Text className={style.workoutCardText}>{workoutDescription}</Card.Text>
+              }
+              <Card.Text>Exercises:</Card.Text>
+              <ul className={style.exerciseList}>
+                {workout.selectedWorkout.data.Workout.exercise.map((exercise) => 
+                  <div key={exercise.id}>
+                    <ExerciseItem
+                      key={exercise.id}
+                      exerciseId={exercise.id}
+                      name={exercise.name}
+                      description={exercise.description}
+                      numberOfSets={exercise.number_of_sets}
+                      maximumRepetitions={exercise.maximum_repetitions}
+                      restTime={exercise.rest_time}
+                    />
+                  </div>
+                )}
+              </ul>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 })
 
