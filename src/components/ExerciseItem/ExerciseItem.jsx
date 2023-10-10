e4ef1d3092735c4e9f5d68a97ce29449899e649c
l-Spook-l <uaspookua@gmail.com>
2023-10-10 17:39:16 +0300

    Implemented updating of exercise name

--- src/components/ExerciseItem/ExerciseItem.jsx
+++ src/components/ExerciseItem/ExerciseItem.jsx
@@ -2,7 +2,7 @@ import { observer } from 'mobx-react-lite'
 import React, { useContext, useState } from 'react'
 import { Card, Col, Row } from 'react-bootstrap'
 import { Context } from '../..'
-import { AiFillEdit} from "react-icons/ai";
+import { AiFillEdit, AiOutlineCheck} from "react-icons/ai";
 import style from './ExerciseItem.module.css'
 
 const ExerciseItem = observer(
@@ -11,15 +11,24 @@ const ExerciseItem = observer(
   const { user } = useContext(Context)
   const { workout } = useContext(Context)
 
-  const [editWorkout, setEditWorkout] = useState(false)
+  const [editExercise, setEditExercise] = useState(false)
   
+  const [exerciseName, setExerciseName] = useState(name)
+  const [exerciseDescription, setExerciseDescription] = useState(description)
+  const [exerciseNumberOfSets, setExerciseNumberOfSets] = useState(numberOfSets)
+  const [exerciseMaximumRepetitions, setExerciseMaximumRepetitions] = useState(maximumRepetitions)
+  const [exerciseRestTime, setExerciseRestTime] = useState(restTime)
+
   return (
     <Card>
       <Card.Body>
         <div className={style.titleSection}>
-          <h3>{name}</h3>
+          {editExercise
+          ? <input type="text" value={exerciseName} onChange={(el) => setExerciseName(el.target.value)} />
+          : <h3>{name}</h3>
+          }
           {(user.user.id === workout.selectedWorkout.data.Workout.user_id) && 
-            <button onClick={() => setEditWorkout(true)}><AiFillEdit/></button>
+            <button onClick={() => setEditExercise(true)}><AiFillEdit/></button>
           }
         </div>
         <p>description: {description}</p>
