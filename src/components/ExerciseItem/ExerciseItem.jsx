da0939d92d824f6a70cbf57872451034d20bcb4a
l-Spook-l <uaspookua@gmail.com>
2023-10-14 17:47:06 +0300

    Implemented retrieval of an array of sets for all exercises

--- src/components/ExerciseItem/ExerciseItem.jsx
+++ src/components/ExerciseItem/ExerciseItem.jsx
@@ -1,73 +1,41 @@
 import { observer } from 'mobx-react-lite'
-import React, { useContext, useState } from 'react'
-import { Card, Col, Row } from 'react-bootstrap'
+import React, { useContext } from 'react'
 import { Context } from '../..'
-import { AiFillEdit, AiOutlineCheck} from "react-icons/ai";
 import style from './ExerciseItem.module.css'
-import { updateExercise } from '../../http/workoutAPI';
 
-const ExerciseItem = observer(
-  ({ exerciseId, name, description, numberOfSets, maximumRepetitions, restTime }) => {
-  
-  const { user } = useContext(Context)
-  const { workout } = useContext(Context)
-
-  const [editExercise, setEditExercise] = useState(false)
-  
-  const [exerciseName, setExerciseName] = useState(name)
-  const [exerciseDescription, setExerciseDescription] = useState(description)
-  const [exerciseNumberOfSets, setExerciseNumberOfSets] = useState(numberOfSets)
-  const [exerciseMaximumRepetitions, setExerciseMaximumRepetitions] = useState(maximumRepetitions)
-  const [exerciseRestTime, setExerciseRestTime] = useState(restTime)
-
-  const updateParamExercise = () => {
-    updateExercise(exerciseId, exerciseName, exerciseDescription, exerciseNumberOfSets, exerciseMaximumRepetitions, exerciseRestTime)
-    setEditExercise(false)
-  }
+const ExerciseInfo = observer(({ exercise, sets }) => {
+  const {user} = useContext(Context)
 
   return (
-    <Card>
-      <Card.Body>
-        <div className={style.titleSection}>
-          {editExercise
-          ? <input type="text" value={exerciseName} onChange={(el) => setExerciseName(el.target.value)} />
-          : <h3>{exerciseName}</h3>
-          }
-          {user.isAuth && 
-            (user.user.id === workout.selectedWorkout.data.Workout.user_id) && 
-              (editExercise
-              ? <button onClick={() => updateParamExercise()}><AiOutlineCheck/></button>
-              : <button onClick={() => setEditExercise(true)}><AiFillEdit/></button>)
-          }
+    <div className={style.container}>
+      <p>{exercise.name}</p>
+      <p>{exercise.description}</p>
+      <div className={style.exerciseSection}>
+        <div className={style.exerciseTitle}>
+          <div>
+            <p>Sets</p>
+            <p>{exercise.number_of_sets}</p>
+          </div>
+          <div>
+            <p>Repetitions</p>
+            <p>{exercise.maximum_repetitions}</p>
+          </div>
+          <div>
+            <p>Rest time</p>
+            <p>{exercise.rest_time}</p>
+          </div>
         </div>
-        <p>description:</p>
-        {editExercise
-          ? <input type="text" value={exerciseDescription} onChange={(el) => setExerciseDescription(el.target.value)} />
-          : <h3>{exerciseDescription}</h3>
-          }
-        <Row>
-          <Col md={4}>numberOfSets: 
-            {editExercise
-            ? <input type="text" value={exerciseNumberOfSets} onChange={(el) => setExerciseNumberOfSets(el.target.value)} />
-            : exerciseNumberOfSets
-            }
-          </Col>
-          <Col md={4}>maximumRepetitions: 
-            {editExercise
-              ? <input type="text" value={exerciseMaximumRepetitions} onChange={(el) => setExerciseMaximumRepetitions(el.target.value)} />
-              : exerciseMaximumRepetitions
-            }
-          </Col>
-          <Col md={4}>restTime: 
-            {editExercise
-              ? <input type="text" value={exerciseRestTime} onChange={(el) => setExerciseRestTime(el.target.value)} />
-              : exerciseRestTime
-            }
-          </Col>
-        </Row>
-      </Card.Body>
-    </Card>
+        <div className={style.setSection}>
+          {sets.map((set) => 
+            <div  key={set.Set.id}>
+              <p>{set.Set.count}</p>
+              <p>{set.Set.weight}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   )
 })
 
-export default ExerciseItem
+export default ExerciseInfo
