29d20f44e675693146d809d3ac08cc624a50cc35
l-Spook-l <uaspookua@gmail.com>
2023-10-15 20:18:13 +0300

    Implemented resetting of necessary fields for proper display

--- src/components/ExerciseItem/ExerciseItem.jsx
+++ src/components/ExerciseItem/ExerciseItem.jsx
@@ -1,11 +1,42 @@
 import { observer } from 'mobx-react-lite'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Context } from '../..'
 import style from './ExerciseItem.module.css'
 import { Card, Col, Container, Row } from 'react-bootstrap'
+import { updateSet } from '../../http/workoutAPI'
 
 const ExerciseInfo = observer(({ exercise, sets }) => {
   const {user} = useContext(Context)
+
+  // делаем новые обькты, а не ссылки на - sets
+  const [oldSets, setOldSets] = useState(JSON.parse(JSON.stringify(sets)))
+  const [newSets, setNewSets] = useState(JSON.parse(JSON.stringify(sets.map((el) => ({ "Set": { ...el.Set, "count": 0, "weight": 0 } })))))
+
+
+  useEffect(() => {
+    setOldSets(JSON.parse(JSON.stringify(sets)));
+    setNewSets(JSON.parse(JSON.stringify(sets.map((el) => ({ "Set": { ...el.Set, "count": 0, "weight": 0 } })))));
+    console.log('---------------------------')
+  }, [sets]);
+
+  //console.log('newSets', newSets)
+
+  const changeSet = (setId, index) => {
+    console.log('click save id', setId)
+    const updateSetData = [...newSets]
+    updateSetData[index].Set['count'] = 10
+    // console.log('updateSet', updateSetData)
+    // console.log('oldSets', oldSets)
+    setNewSets(updateSetData)
+    updateSet(setId, 10, 0)
+  }
+
+  const saveExercise = () => {
+    console.log('save exercise')
+  }
+
+  console.log('newSets', newSets)
+
 
   return (
     <Container className={style.container}>
@@ -27,7 +58,7 @@ const ExerciseInfo = observer(({ exercise, sets }) => {
           </Col>
         </Card.Title>
         <div className={style.setSection}>
-          {sets.map((set) => 
+          {oldSets.map((set, index) => 
             <div key={set.Set.id} className={style.set}>
               <Col className={style.blockInSet}>
                 <p>{set.Set.count}</p>
@@ -35,17 +66,17 @@ const ExerciseInfo = observer(({ exercise, sets }) => {
                 <p>{set.Set.weight} kg</p>
               </Col>
               <Col className={style.blockInSet}>
-                <p>{set.Set.count}</p>
+                <p>{newSets[index].Set.count}</p>
                 /
-                <p>{set.Set.weight} kg</p>
+                <p>{newSets[index].Set.weight} kg</p>
               </Col>
               <Col className={style.blockInSet}>
-                <button >Save</button>
+                <button onClick={() => changeSet(set.Set.id, index)} >Save</button>
               </Col>
             </div>
-          )}
+          )}          
         </div>
-        <button className={style.buttonSave}>Save exercise</button>
+        <button className={style.buttonSave} onClick={saveExercise}>Save exercise</button>
       </Card>
     </Container>
   )
