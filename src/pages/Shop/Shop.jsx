7f8aa1497b921a10b7db6c7a9330cda2d972ce78
l-Spook-l <uaspookua@gmail.com>
2023-10-18 20:56:04 +0300

    A placeholder page for the store has been created

---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Button } from 'react-bootstrap';
+import { AiFillSetting } from 'react-icons/ai';
+import style from './Shop.module.css'
+
+const Shop = () => {
+  return (
+    <Container className={style.container}>
+      <p className={style.setIcon}><AiFillSetting/></p>
+      <h1>The store is under development.</h1>
+      <p>
+      Our store is in the development stage. Register to receive notifications about the store's launch!
+      </p>
+      <Button variant="dark">Register</Button>
+  </Container>
+  )
+}
+
+export default Shop
