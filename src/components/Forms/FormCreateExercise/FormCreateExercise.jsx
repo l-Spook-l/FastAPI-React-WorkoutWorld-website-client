c61afd2284717ebf16e47dfc69dacf280ab74242
l-Spook-l <uaspookua@gmail.com>
2023-10-20 22:00:19 +0300

    Added: when adding a new exercise, new sets are created for the user.

--- src/components/Forms/FormCreateExercise/FormCreateExercise.jsx
+++ src/components/Forms/FormCreateExercise/FormCreateExercise.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Card, Col, Form, Row } from 'react-bootstrap'
+import { createExercise, createSet } from '../../../http/workoutAPI';
+import { Context } from '../../..';
 
 // const FormCreateExercise = ({ exercise, onChange, onRemove }) => {
-const FormCreateExercise = ({ showForm }) => {
-  //const { user } = useContext(Context)
+const FormCreateExercise = ({ showForm, workoutId }) => {
+  const { user } = useContext(Context)
 
-  const [confirm, setConfirm] = useState(false)
+  const [confirm, setConfirm] = useState(true)
 
   // Общая проверка валидации формы
   const [formValidError, setFormValidError] = useState(false);
@@ -26,53 +28,40 @@ const FormCreateExercise = ({ showForm }) => {
   const exerciseChange = (index, property, value) => {
     const updatedExercises = [...newExercises];
     updatedExercises[index][property] = value;
-    setNewExercises((prevData) => ({ ...prevData, exercises: updatedExercises }));
+    setNewExercises(updatedExercises)
   };
 
-  // создаем обьект тренировки
-  // const [workoutData, setWorkoutData] = useState({
-  //   title: '',
-  //   description: '',
-  //   totalTime: '1',
-  //   exercises: [],
-  // });
-
-  // // добавляем значение в обьект workoutData меняя сосояние
-  // // ...prevData - добавляем что уже было
-  // const inputChange = (el) => {
-  //   const { name, value } = el.target;
-  //   setWorkoutData((prevData) => ({ ...prevData, [name]: value }));  // [name] - ключем будет именно значение в переменной, а без [] будет - 'name'
-  // };
-
-  // удалить упражнение
+// удалить упражнение вернуться и оптимизировать !!!!!!!!!!!!!!!!!
   const removeExercise = (index) => {
     const updatedExercises = [...newExercises]; 
-    console.log('index ', index)
-    console.log('exercises 1', updatedExercises)
     updatedExercises.splice(index, 1);
-    console.log('exercises 2', updatedExercises)
     setNewExercises(updatedExercises)
-    updatedExercises.length < 1 && setConfirm(false)  // если ничего нет в массив кнопка не активна
+    updatedExercises.length < 1 && setConfirm(false)  // если ничего нет в массиве кнопка не активна
   };
 
-  // // сохраняем тренировку
-  // const submitCreateWorkout = () => {
-  //   const checkDataWorkout = Object.values(workoutData).slice(0, -1).every((value) => value !== '')
-  //   const checkDataExercise = workoutData.exercises.every((el) => Object.values(el).slice(0, -2).every((value) => value !== ''))
+  // сохраняем тренировку
+  const submitNewExercise = () => {
+    const checkDataExercise = newExercises.every((el) => Object.values(el).slice(0, -2).every((value) => value !== ''))
 
-  //   if (!checkDataWorkout || !checkDataExercise) {
-  //     setFormValidError(true) 
-  //   } else {
-  //     setFormValidError(false)
-  //     createWorkout(workoutData.title, user.user.id, workoutData.description, 'medium', workoutData.totalTime)
-  //     .then((data) => 
-  //     workoutData.exercises.map((exercise) => 
-  //       createExercise( exercise.name, data.workout_ID, exercise.description, exercise.sets, exercise.maximumRepetitions, exercise.restTime, exercise.photo, exercise.video)
-  //     ))
-  //   }
-  // };
-
-  console.log('FormCreateExercise newExercises', newExercises)
+    if (!checkDataExercise) {
+      setFormValidError(true) 
+    } else {
+      setFormValidError(false)
+      
+      newExercises.map((exercise) => 
+        createExercise( 
+          exercise.name, 
+          workoutId, 
+          exercise.description, 
+          exercise.sets, 
+          exercise.maximumRepetitions, 
+          exercise.restTime, 
+          exercise.photo, 
+          exercise.video).then((data) => createSet(exercise.sets, data.exercise_ID, user.user.id, 0, 0))
+      )
+      window.location.reload()
+    }
+  };
 
   return (
     <Form>
@@ -95,40 +84,22 @@ const FormCreateExercise = ({ showForm }) => {
                     />
                   </Col>
                 </Form.Group>
-                <Button variant="danger" onClick={() => removeExercise(index)}>
-                  Remove Exercise
-                </Button>
-              </Card.Body>
-            </Card>
-          )}
-          <Button variant="primary" onClick={addExercise}>
-            Add Exercise
-          </Button>
-          {formValidError && <p>Fill in all fields!</p>}
-        </div>
-      }
-      {/* <Form>
-        <h2>Exercises</h2>
-        {workoutData.exercises.map((exercise, index) => 
-          <Card key={index}>
-            <Card.Body>
-              
 
-              <Form.Group as={Row} className="mb-3">
-                <Form.Label column sm="2">Description</Form.Label>
-                <Col md={5}>
-                  <Form.Control
-                    as="textarea"
-                    rows={3}
-                    placeholder="Enter description"
-                    name="description"
-                    value={exercise.description}
-                    onChange={(e) => exerciseChange(index, 'description', e.target.value)}
-                  />
-                </Col>
-            </Form.Group>
+                <Form.Group as={Row} className="mb-3">
+                  <Form.Label column sm="2">Description</Form.Label>
+                  <Col md={5}>
+                    <Form.Control
+                      as="textarea"
+                      rows={3}
+                      placeholder="Enter description"
+                      name="description"
+                      value={exercise.description}
+                      onChange={(e) => exerciseChange(index, 'description', e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
 
-              <Form.Group as={Row} className="mb-3">
+                <Form.Group as={Row} className="mb-3">
                 <Form.Label column sm="2">
                   Sets
                 </Form.Label>
@@ -198,19 +169,21 @@ const FormCreateExercise = ({ showForm }) => {
                 </Col> Optional
               </Form.Group>
 
-              
-            </Card.Body>
-          </Card>
-        )}
-
-        
-
-        <Button className="" disabled={!confirm} variant={confirm ? undefined : "success"} onClick={submitCreateWorkout}>
-          Create Workout
-        </Button>
-        
-
-      </Form> */}
+                <Button variant="danger" onClick={() => removeExercise(index)}>
+                  Remove Exercise
+                </Button>
+              </Card.Body>
+            </Card>
+          )}
+          <Button variant="primary" onClick={addExercise}>
+            Add Exercise
+          </Button>
+          <Button className="" disabled={!confirm} variant={confirm ? undefined : "success"} onClick={submitNewExercise}>
+            Save
+          </Button>
+          {formValidError && <p>Fill in all fields!</p>}
+        </div>
+      }
     </Form>
   )
 }
