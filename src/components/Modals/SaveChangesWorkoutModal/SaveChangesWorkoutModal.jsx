6498d6daebd501f59aa43a88ed03e3e25b2c8369
l-Spook-l <uaspookua@gmail.com>
2023-10-24 23:35:16 +0300

    A modal window for confirming workout changes has been created

---
+++ src/components/Modals/SaveChangesWorkoutModal/SaveChangesWorkoutModal.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button, Modal } from 'react-bootstrap';
+
+const SaveChangesWorkoutModal = ({ show, onClose, saveChanges }) => {
+  return (
+    <Modal show={show} onHide={onClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Warning</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+      Are you sure you want to make changes to this workout?
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose}>
+          No
+        </Button>
+        <Button variant="success" onClick={saveChanges}>
+          Yes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default SaveChangesWorkoutModal
