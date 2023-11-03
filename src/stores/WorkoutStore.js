3d338450361c2f26d60fd4a8c05ca60198577fd5
l-Spook-l <uaspookua@gmail.com>
2023-11-03 17:39:06 +0200

    Workout difficulties have been added

--- src/stores/WorkoutStore.js
+++ src/stores/WorkoutStore.js
@@ -7,6 +7,13 @@ export default class WorkoutStore {
     this._selectedWorkout = []
     this._userWorkouts = []
     this._addedWorkouts = []
+    
+    this._difficulties = []
+    this._selectedDifficulty = []
+
+    this._searchWorkouts = []  // массив для поиска тренировок по названию
+    this._selectedSearchWorkouts = ''
+
     this._editWorkout = false
 
     this._page = 1
@@ -29,9 +36,25 @@ export default class WorkoutStore {
   setAddedWorkouts(addedWorkouts) {
     this._addedWorkouts = addedWorkouts
   }
+  
+  setDifficulties(difficulties) {
+    this._difficulties = difficulties
+  }
+  setSelectedDifficulty(difficulty) {
+    this._selectedDifficulty = difficulty
+  }
+
+  setSearchWorkouts(workouts){
+    this._searchWorkouts = workouts
+  }
+  setSelectedSearchWorkouts(workout) {
+    this._selectedSearchWorkouts = workout
+  }
+
   setEditWorkout(bool) {
     this._editWorkout = bool
   }
+
   setPage(page) {
     this._page = page
   }
@@ -57,9 +80,25 @@ export default class WorkoutStore {
   get addedWorkouts() {
     return this._addedWorkouts
   }
+
+  get difficulties() {
+    return this._difficulties
+  }
+  get selectedDifficulty() {
+    return this.selectedDifficulty
+  }
+  
+  get searchWorkouts() {
+    return this._searchWorkouts
+  }
+  get selectedSearchWorkouts() {
+    return this._selectedSearchWorkouts
+  }
+
   get editWorkout() {
     return this._editWorkout
   }
+
   get page() {
     return this._page
   }
