69dfa47cc4a9c71c1a3aabdb9e80330ed471b98a
l-Spook-l <uaspookua@gmail.com>
2023-11-11 23:33:53 +0200

    A filter for the workout status has been created

---
+++ src/components/Filters/IsPublicBar/IsPublicBar.jsx
@@ -0,0 +1,67 @@
+import { observer } from "mobx-react-lite";
+import React, { useContext, useState } from "react";
+import { Accordion } from "react-bootstrap";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import style from "./IsPublicBar.module.css";
+
+const IsPublicBar = observer(({ statusWorkout }) => {
+
+  const [show, setShow] = useState(true);
+  const [selectedStatus, setSelectedStatus] = useState();
+
+  const statusWorkoutList = [
+    {
+      id: 1,
+      status: 'Public',
+      value: true
+    },
+    {
+      id: 2,
+      status: 'Non-public',
+      value: false
+    },
+  ]
+
+  const statusChange = (status) => {
+    if (selectedStatus === status.id) {
+      setSelectedStatus()
+      statusWorkout();
+    } else {
+      setSelectedStatus(status.id)
+      statusWorkout(status.value);
+    }
+  };
+
+  return (
+    <Accordion bsPrefix="my-accordion" className={style.accordion}>
+      <Accordion.Item bsPrefix="my-accordionItem" eventKey="0">
+        <Accordion.Header bsPrefix="my-accordionHeader" onClick={() => setShow(!show)}>
+          <span className={style.accordionHeader}>Public</span>
+          <span className={style.arrow}>
+            {show ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          </span>
+        </Accordion.Header>
+        <Accordion.Body bsPrefix="my-accordionBody" className={style.accordionBody}>
+          {statusWorkoutList.map((status) => 
+            <div key={status.id} className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input me-3"
+                checked={selectedStatus === status.id}
+                onChange={() => statusChange(status)}
+              />
+              <label
+                className={style.labelCheck}
+                onClick={() => statusChange(status)}
+              >
+                {status.status}
+              </label>
+            </div>
+          )}
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+});
+
+export default IsPublicBar;
