39e5ea3d395f5d9711713618200a765824ff90ee
l-Spook-l <uaspookua@gmail.com>
2023-11-14 21:58:25 +0200

    Password reset page has been created

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import {ACTIVE_WORKOUT_ROUTE, CREATE_WORKOUT_ROUTE, MAIN_ROUTE, PROFILE_ROUTE, SHOP_ROUTE, WORKOUTS_ROUTE, WORKOUT_ROUTE } from './utils/consts'
+import {ACTIVE_WORKOUT_ROUTE, CREATE_WORKOUT_ROUTE, MAIN_ROUTE, PROFILE_ROUTE, SHOP_ROUTE, WORKOUTS_ROUTE, WORKOUT_ROUTE, RESET_USER_PASSWORD_ROUTE } from './utils/consts'
 import MainPage from './pages/MainPage/MainPage'
 import WorkoutsPage from './pages/WorkoutsPage/WorkoutsPage'
 import ProfilePage from './pages/Profile/Profile'
@@ -6,6 +6,7 @@ import WorkoutPage from './pages/WorkoutPage/WorkoutPage'
 import CreateWorkoutPage from './pages/CreateWorkoutPage/CreateWorkoutPage'
 import ActiveWorkoutPage from './pages/ActiveWorkoutPage/ActiveWorkoutPage'
 import Shop from './pages/Shop/Shop'
+import ResetUserPassword from './pages/ResetUserPassword/ResetUserPassword'
 
 export const authRoutes = [
   {
@@ -39,4 +40,8 @@ export const publicRoutes = [
     path: SHOP_ROUTE,
     Component: Shop,
   },
+  {
+    path: RESET_USER_PASSWORD_ROUTE + "/:token",
+    Component: ResetUserPassword
+  }
 ]
