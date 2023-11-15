5b9d5316773e5665d195d67644080172882bca32
l-Spook-l <uaspookua@gmail.com>
2023-11-15 12:02:55 +0200

    Created the page for group workouts

--- src/pages/GroupWorkouts/GroupWorkouts.jsx
+++ src/pages/GroupWorkouts/GroupWorkouts.jsx
@@ -5,7 +5,7 @@ import { Container } from 'react-bootstrap'
 const GroupWorkouts = () => {
   return (
     <div className={style.mainBlock}>
-      <Container className={style.container}>
+      <Container>
         <p className={style.text}>At the moment, enrollment for fitness groups is not available. Register and enable notifications to stay informed about our updates!</p>
       </Container>
     </div>
