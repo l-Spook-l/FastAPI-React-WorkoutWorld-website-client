dd9b75f5e9ddc5140261aa29fceeb09146d60970
l-Spook-l <uaspookua@gmail.com>
2023-11-15 11:57:25 +0200

    Added a router for the group workout page

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import {ACTIVE_WORKOUT_ROUTE, CREATE_WORKOUT_ROUTE, MAIN_ROUTE, PROFILE_ROUTE, SHOP_ROUTE, WORKOUTS_ROUTE, WORKOUT_ROUTE, RESET_USER_PASSWORD_ROUTE } from './utils/consts'
+import {ACTIVE_WORKOUT_ROUTE, CREATE_WORKOUT_ROUTE, MAIN_ROUTE, PROFILE_ROUTE, SHOP_ROUTE, WORKOUTS_ROUTE, WORKOUT_ROUTE, RESET_USER_PASSWORD_ROUTE, GROUP_WORKOUTS_ROUTE } from './utils/consts'
 import MainPage from './pages/MainPage/MainPage'
 import WorkoutsPage from './pages/WorkoutsPage/WorkoutsPage'
 import ProfilePage from './pages/Profile/Profile'
@@ -7,6 +7,7 @@ import CreateWorkoutPage from './pages/CreateWorkoutPage/CreateWorkoutPage'
 import ActiveWorkoutPage from './pages/ActiveWorkoutPage/ActiveWorkoutPage'
 import Shop from './pages/Shop/Shop'
 import ResetUserPassword from './pages/ResetUserPassword/ResetUserPassword'
+import GroupWorkouts from './pages/GroupWorkouts/GroupWorkouts'
 
 export const authRoutes = [
   {
@@ -43,5 +44,9 @@ export const publicRoutes = [
   {
     path: RESET_USER_PASSWORD_ROUTE + "/:token",
     Component: ResetUserPassword
+  },
+  {
+    path: GROUP_WORKOUTS_ROUTE,
+    Component: GroupWorkouts
   }
 ]
