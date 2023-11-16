172b153308f32494b54f4e0cfcdcae94da2c47a1
l-Spook-l <uaspookua@gmail.com>
2023-11-16 15:44:22 +0200

    Token error handling has been implemented

--- src/pages/ResetUserPassword/ResetUserPassword.jsx
+++ src/pages/ResetUserPassword/ResetUserPassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Col, Container, Form, InputGroup, Row } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
@@ -7,14 +7,42 @@ import { resetUserPassword } from '../../http/userAPI';
 
 const ResetUserPassword = () => {
   const token = useParams()
-  console.log('token', token.token)
 
   const [password, setPassword] = useState("")
   const [showPassword, setShowPassword] = useState(false)
+
+  const [tokenError, setTokenError] = useState(false)
+
+  const [formValid, setFormValid] = useState(false)
+  const [passwordDirty, setPasswordDirty] = useState(false)
   const [passwordError, setPasswordError] = useState("Password cannot be empty")
+
+  useEffect(() => {
+    if (passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [passwordError])
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case "password":
+        setPasswordDirty(true);
+        break;
+    }
+  }
 
   const passwordHandler = (e) => {
     setPassword(e.target.value);
+    if (e.target.value.length < 8) {
+      setPasswordError("Password must be longer than 8 characters");
+      if (!e.target.value) {
+        setPasswordError("Password cannot be empty");
+      }
+    } else {
+      setPasswordError("");
+    }
   }
 
   const toggleShowPassword = () => {
@@ -22,17 +50,31 @@ const ResetUserPassword = () => {
   }
 
   const resetPassword  = () => {
-    resetUserPassword(token.token, password)
+    resetUserPassword(token.token, password).catch((error) => {
+      if (error.response.data.detail === 'RESET_PASSWORD_BAD_TOKEN' ) {
+        setTokenError(true)
+      } else {
+        setTokenError(false)
+      }
+    })
   }
 
   return (
     <Container fluid className={style.container}>
     <div>
-      <Form className={style.formBlock}>
+        <Form className={style.formBlock}>
+          {tokenError &&  (
+          <p className="text-danger">
+            Sorry, there was an error while resetting your password. 
+            Your password reset link might be expired or invalid. 
+            Please try requesting a password reset again or contact support for assistance.
+          </p>
+        )}
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <InputGroup>
               <Form.Control
+                onBlur={(e) => blurHandler(e)}
                 name="password"
                 type={showPassword ? 'text' : 'password'}
                 placeholder="Enter password"
@@ -43,11 +85,14 @@ const ResetUserPassword = () => {
               {showPassword ? <AiOutlineEye style={{fontSize: '20px'}}/> :<AiOutlineEyeInvisible style={{fontSize: '20px'}}/> }
               </Button>
             </InputGroup>
+            {passwordDirty && passwordError && (
+              <Form.Text className="text-danger">{passwordError}</Form.Text>
+            )}
           </Form.Group>
         
           <Row className="mt-2">
             <Col className="d-flex justify-content-end">
-              <Button variant="success" onClick={() => resetPassword()}>
+              <Button variant="success" disabled={!formValid} onClick={() => resetPassword()}>
                 Reset password
               </Button>
             </Col>
