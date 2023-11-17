2a7b26069ea252707e1b7bb21e2c8bce7885706e
l-Spook-l <uaspookua@gmail.com>
2023-11-17 14:21:56 +0200

    The deletion window has been made more versatile

---
+++ src/components/Modals/DeleteModal/DeleteModal.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button, Modal } from 'react-bootstrap';
+
+const DeleteModal = ({ show, onClose, deleteValue, value }) => {
+  return (
+    <Modal show={show} onHide={onClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Warning</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+      Are you sure you want to delete this {value}?
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose}>
+          No
+        </Button>
+        <Button variant="success" onClick={deleteValue}>
+          Yes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default DeleteModal
