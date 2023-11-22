921622103fc8bd811f9e46cd93789cee15b92b2a
l-Spook-l <uaspookua@gmail.com>
2023-11-22 15:37:01 +0200

    The photo deletion is now implemented as a separate API for exercise editing.

--- src/http/workoutAPI.js
+++ src/http/workoutAPI.js
@@ -1,16 +1,16 @@
 import { $authHost, $host } from "./index"
 
 
-export const fetchWorkouts = async(skip, difficulty, name) => {
+export const fetchWorkouts = async(skip, difficulty, limit, name) => {
   console.log('fetchWorkouts difficulty', difficulty) 
   // paramsSerializer - чтобы не было [] в строке запроса
-  const response = await $host.get('workouts/', {params: {skip:skip, name:name, difficulty:difficulty}, paramsSerializer: {indexes: null}})
+  const response = await $host.get('workouts/', {params: {skip:skip, name:name, difficulty:difficulty, limit:limit}, paramsSerializer: {indexes: null}})
   console.log('fetchWorkouts response', response)
   return response.data
 }
 
-export const fetchOneWorkout = async(workout_id) => {
-  const response = await $host.get(`workouts/workout/${workout_id}`)
+export const fetchOneWorkout = async(workout_id, user_id) => {
+  const response = await $host.get(`workouts/workout/${workout_id}`, {params: {user_id:user_id}})
   console.log('fetchOneWorkout response', response)
   return response.data
 }
@@ -30,8 +30,8 @@ export const fetchSets = async(user_id, exercise_ids) => {
   return response.data
 }
 
-export const fetchAddUserWorkout = async(user_id) => {
-  const response = await $authHost.get(`workouts/get-user-added-workouts/${user_id}`)
+export const fetchAddUserWorkout = async(user_id, name, difficulty, skip, limit) => {
+  const response = await $authHost.get(`workouts/get-user-added-workouts/${user_id}`, {params : {name:name, difficulty:difficulty, skip:skip, limit:limit}, paramsSerializer: {indexes: null}})
   console.log('fetchAddUserWorkout response', response)
   return response.data
 }
@@ -63,7 +63,7 @@ export const createExercise = async(name, workout_id, description, number_of_set
   formData.append('maximum_repetitions', maximum_repetitions)
   formData.append('rest_time', rest_time)
   formData.append('video', video)
-  Object.values(photos).forEach((photo) => formData.append('photos', photo))
+  photos && Object.values(photos).forEach((photo) => formData.append('photos', photo))
   const response = await $authHost.post('workouts/create_exercise/', formData, 
   )
   console.log('createExercise response', response)
@@ -110,14 +110,26 @@ export const deleteCreatedWorkout = async(workout_id) => {
   return response.data
 }
 
+export const deleteExercise = async(exercise_id) => {
+  const response = await $authHost.delete(`workouts/delete/exercise?exercise_id=${exercise_id}`)
+  console.log('deleteExercise response', response)
+  return response.data
+}
+
 export const deleteAddedWorkout = async(workout_id, user_id) => {
   const response = await $authHost.delete(`workouts/delete/added-workout?workout_id=${workout_id}&user_id=${user_id}`)
   console.log('deleteAddedWorkout response', response)
   return response.data
 }
 
-export const deleteAddedSets = async(exercise_id, user_id) => {
-  const response = await $authHost.delete(`workouts/delete/added-sets?exercise_id=${exercise_id}&user_id=${user_id}`)
+export const deleteSets = async(exercise_id, user_id) => {
+  const response = await $authHost.delete(`workouts/delete/sets?exercise_id=${exercise_id}&user_id=${user_id}`)
   console.log('deleteAddedSets response', response)
+  return response.data
+}
+
+export const deletePhotos = async(exercise_id, photo_ids) => {
+  const response = await $host.delete(`workouts/delete/photo`, {params: {exercise_id:exercise_id, photo_ids:photo_ids}, paramsSerializer: {indexes: null}})
+  console.log('deletePhotos response', response)
   return response.data
 }
