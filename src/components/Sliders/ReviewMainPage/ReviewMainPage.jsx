d1a6bfb2103890e5c5ba8c2dad09a0ac6c2973c7
l-Spook-l <uaspookua@gmail.com>
2023-11-26 15:49:20 +0200

    Implemented acceptable responsive styles.

--- src/components/Sliders/ReviewMainPage/ReviewMainPage.jsx
+++ src/components/Sliders/ReviewMainPage/ReviewMainPage.jsx
@@ -6,17 +6,18 @@ const ReviewMainPage = () => {
   return (
     <Carousel className={style.carousel}>
       <Carousel.Item className={style.carouselItem}>
-      <Carousel.Caption className={style.carouselCaption}>
+      <Carousel.Caption>
       <p className={style.text}>
-          I absolutely love WorkoutWorld! Their wide range of sports goods and 
-          expertly designed workouts have helped me achieve my fitness goals. 
+        I absolutely love WorkoutWorld! Their wide range of sports goods and expertly 
+        designed workouts have helped me achieve my fitness goals. Their dedication to 
+        quality and customer satisfaction is truly commendable.
         </p>
         - Olivia
       </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item className={style.carouselItem}>
-        <Carousel.Caption className={style.carouselCaption}>
+        <Carousel.Caption>
         <p className={style.text}>
           WorkoutWorld is the best place to get everything you need for a 
           great workout! Their knowledgeable staff helped me create a personalized 
@@ -28,7 +29,7 @@ const ReviewMainPage = () => {
       </Carousel.Item>
 
       <Carousel.Item className={style.carouselItem}>
-        <Carousel.Caption className={style.carouselCaption}>
+        <Carousel.Caption>
         <p className={style.text}>
           I'm so grateful for WorkoutWorld! Their incredible workout creation and 
           wide range of sports goods have helped me achieve my fitness goals. 
