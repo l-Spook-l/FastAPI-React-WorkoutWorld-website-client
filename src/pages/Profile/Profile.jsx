2c49512583c83c01425b61b285003a54077c78a2
l-Spook-l <uaspookua@gmail.com>
2023-11-26 20:18:33 +0200

    Made the component more versatile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Alert, Col, Container, Nav, Row } from 'react-bootstrap'
+import { Alert, Container } from 'react-bootstrap'
 import style from './Profile.module.css'
 import { observer } from 'mobx-react-lite'
 import { Context } from "../..";
@@ -110,64 +110,6 @@ const Profile = observer(() => {
           </Alert>
         </div>
         <div className={style.contentBlock}>{view}</div>
-        {/* </div> */}
-        {/* <Row>
-          <Col md={3} className={style.blockWithMenu}>
-            <Nav className="d-flex flex-column">
-              <Alert
-                className={`${style.alertMenu} ${activeTab === "userInfo" ? style.alertMenuActive : ""}`}
-                onClick={() => blurHandler("userInfo")}
-              >
-                {user.user.first_name} {user.user.last_name}
-                <span>{user.user.email}</span>
-              </Alert>
-              <hr />
-              <Alert
-                className={`${style.alertMenu} ${activeTab === "createdWorkouts" ? style.alertMenuActive : ""}`}
-                onClick={() => blurHandler("createdWorkouts")}
-              >
-                Created workouts
-              </Alert>
-              <Alert
-                className={`${style.alertMenu} ${activeTab === "addedWorkouts" ? style.alertMenuActive : ""}`}
-                onClick={() => blurHandler("addedWorkouts")}
-              >
-                Added workouts
-              </Alert>
-              <Alert
-                className={`${style.alertMenu} ${activeTab === "basket" ? style.alertMenuActive : ""}`}
-                onClick={() => blurHandler("basket")}
-              >
-                Basket (Under development)
-              </Alert>
-              <Alert
-                className={`${style.alertMenu} ${activeTab === "wishlist" ? style.alertMenuActive : ""}`}
-                onClick={() => blurHandler("wishlist")}
-              >
-                Wishlist (Under development)
-              </Alert>
-              <Alert
-                className={`${style.alertMenu} ${activeTab === "subscribe" ? style.alertMenuActive : ""}`}
-                onClick={() => blurHandler("subscribe")}
-              >
-                Subscriptions (Under development)
-              </Alert>
-              <Alert
-                className={`${style.alertMenu} ${activeTab === "wallet" ? style.alertMenuActive : ""}`}
-                onClick={() => blurHandler("wallet")}
-              >
-                Wallet (Under development)
-              </Alert>
-              <Alert
-                className={`${style.alertMenu} ${activeTab === "orders" ? style.alertMenuActive : ""}`}
-                onClick={() => blurHandler("orders")}
-              >
-                My orders (Under development)
-              </Alert>
-            </Nav>
-          </Col>
-          <Col md={9} className={style.contentBlock}>{view}</Col>
-        </Row> */}
       </Container>
     </div>
   )
