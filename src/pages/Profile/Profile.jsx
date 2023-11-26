b375417883c3c0a2997ef5744502ec4d21f9a208
l-Spook-l <uaspookua@gmail.com>
2023-11-26 20:17:41 +0200

    Made the component more versatile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -9,111 +9,167 @@ import UserInfo from '../../components/UserInfo/UserInfo';
 import AddedWorkouts from '../../components/AddedWorkouts/AddedWorkouts';
 
 const Profile = observer(() => {
-  const { user } = useContext(Context);
+  const { user } = useContext(Context)
 
-  const location = useLocation();
+  const location = useLocation()
 
-  const state = location.state;
+  const state = location.state
 
-  const [activeTab, setActiveTab] = useState("userInfo");
+  const [activeTab, setActiveTab] = useState("userInfo")
 
-  const [view, setView] = useState(<UserInfo />);
+  const [view, setView] = useState(<UserInfo />)
 
   useEffect(() => {
     window.scrollTo(0, 0);
     blurHandler(state);
-  }, [state]);
+  }, [state])
 
   const blurHandler = (tab) => {
-    setActiveTab(tab);
+    setActiveTab(tab)
     switch (tab) {
       case "userInfo":
-        setView(<UserInfo />);
+        setView(<UserInfo />)
         break;
       case "createdWorkouts":
-        setView(<CreatedWorkouts />);
+        setView(<CreatedWorkouts />)
         break;
       case "addedWorkouts":
-        setView(<AddedWorkouts />);
+        setView(<AddedWorkouts />)
         break;
       case "basket":
-        //setView(<Basket />);
+        //setView(<Basket />)
         break;
       case "wishlist":
-        //setView(<Wishlist />);
+        //setView(<Wishlist />)
         break;
       case "subscribe":
-        //setView(<Subscribes />);
+        //setView(<Subscribes />)
         break;
       case "wallet":
-        //setView(<Wallet />);
+        //setView(<Wallet />)
         break;
       case "orders":
-        //setView(<Orders />);
+        //setView(<Orders />)
         break;
     }
-  };
+  }
 
   return (
-    <Container className={style.forContainer}>
-      <Row className="">
-        <Col md={3} className={style.blockWithMenu}>
-          <Nav className="d-flex flex-column">
-            <Alert
-              className={`${style.alertMenu} ${activeTab === "userInfo" ? style.alertMenuActive : ""}`}
-              onClick={() => blurHandler("userInfo")}
-            >
-              {user.user.first_name} {user.user.last_name}
-              <span>{user.user.email}</span>
-            </Alert>
-            <hr />
-            <Alert
-              className={`${style.alertMenu} ${activeTab === "createdWorkouts" ? style.alertMenuActive : ""}`}
-              onClick={() => blurHandler("createdWorkouts")}
-            >
-              Created workouts
-            </Alert>
-            <Alert
-              className={`${style.alertMenu} ${activeTab === "addedWorkouts" ? style.alertMenuActive : ""}`}
-              onClick={() => blurHandler("addedWorkouts")}
-            >
-              Added workouts
-            </Alert>
-            {/* <Alert
-              className={`${style.alertMenu} ${activeTab === "basket" ? style.alertMenuActive : ""}`}
-              onClick={() => blurHandler("basket")}
-            >
-              Basket
-            </Alert>
-            <Alert
-              className={`${style.alertMenu} ${activeTab === "wishlist" ? style.alertMenuActive : ""}`}
-              onClick={() => blurHandler("wishlist")}
-            >
-              Wishlist
-            </Alert>
-            <Alert
-              className={`${style.alertMenu} ${activeTab === "subscribe" ? style.alertMenuActive : ""}`}
-              onClick={() => blurHandler("subscribe")}
-            >
-              Subscriptions
-            </Alert>
-            <Alert
-              className={`${style.alertMenu} ${activeTab === "wallet" ? style.alertMenuActive : ""}`}
-              onClick={() => blurHandler("wallet")}
-            >
-              Wallet
-            </Alert>
-            <Alert
-              className={`${style.alertMenu} ${activeTab === "orders" ? style.alertMenuActive : ""}`}
-              onClick={() => blurHandler("orders")}
-            >
-              My orders
-            </Alert> */}
-          </Nav>
-        </Col>
-        <Col md={9} className={style.contentBlock}>{view}</Col>
-      </Row>
-    </Container>
+    <div className={style.mainBlock}>
+      <Container className={style.forContainer}>
+        <div className={style.blockWithMenu}>
+          <Alert
+            className={`${style.alertMenu} ${activeTab === "userInfo" ? style.alertMenuActive : ""}`}
+            onClick={() => blurHandler("userInfo")}
+          >
+            {user.user.first_name} {user.user.last_name}
+            <span>{user.user.email}</span>
+          </Alert>
+          <hr />
+          <Alert
+            className={`${style.alertMenu} ${activeTab === "createdWorkouts" ? style.alertMenuActive : ""}`}
+            onClick={() => blurHandler("createdWorkouts")}
+          >
+            Created workouts
+          </Alert>
+          <Alert
+            className={`${style.alertMenu} ${activeTab === "addedWorkouts" ? style.alertMenuActive : ""}`}
+            onClick={() => blurHandler("addedWorkouts")}
+          >
+            Added workouts
+          </Alert>
+          <Alert
+            className={`${style.alertMenu} ${activeTab === "basket" ? style.alertMenuActive : ""}`}
+            onClick={() => blurHandler("basket")}
+          >
+            Basket (Under development)
+          </Alert>
+          <Alert
+            className={`${style.alertMenu} ${activeTab === "wishlist" ? style.alertMenuActive : ""}`}
+            onClick={() => blurHandler("wishlist")}
+          >
+            Wishlist (Under development)
+          </Alert>
+          <Alert
+            className={`${style.alertMenu} ${activeTab === "subscribe" ? style.alertMenuActive : ""}`}
+            onClick={() => blurHandler("subscribe")}
+          >
+            Subscriptions (Under development)
+          </Alert>
+          <Alert
+            className={`${style.alertMenu} ${activeTab === "wallet" ? style.alertMenuActive : ""}`}
+            onClick={() => blurHandler("wallet")}
+          >
+            Wallet (Under development)
+          </Alert>
+          <Alert
+            className={`${style.alertMenu} ${activeTab === "orders" ? style.alertMenuActive : ""}`}
+            onClick={() => blurHandler("orders")}
+          >
+            My orders (Under development)
+          </Alert>
+        </div>
+        <div className={style.contentBlock}>{view}</div>
+        {/* </div> */}
+        {/* <Row>
+          <Col md={3} className={style.blockWithMenu}>
+            <Nav className="d-flex flex-column">
+              <Alert
+                className={`${style.alertMenu} ${activeTab === "userInfo" ? style.alertMenuActive : ""}`}
+                onClick={() => blurHandler("userInfo")}
+              >
+                {user.user.first_name} {user.user.last_name}
+                <span>{user.user.email}</span>
+              </Alert>
+              <hr />
+              <Alert
+                className={`${style.alertMenu} ${activeTab === "createdWorkouts" ? style.alertMenuActive : ""}`}
+                onClick={() => blurHandler("createdWorkouts")}
+              >
+                Created workouts
+              </Alert>
+              <Alert
+                className={`${style.alertMenu} ${activeTab === "addedWorkouts" ? style.alertMenuActive : ""}`}
+                onClick={() => blurHandler("addedWorkouts")}
+              >
+                Added workouts
+              </Alert>
+              <Alert
+                className={`${style.alertMenu} ${activeTab === "basket" ? style.alertMenuActive : ""}`}
+                onClick={() => blurHandler("basket")}
+              >
+                Basket (Under development)
+              </Alert>
+              <Alert
+                className={`${style.alertMenu} ${activeTab === "wishlist" ? style.alertMenuActive : ""}`}
+                onClick={() => blurHandler("wishlist")}
+              >
+                Wishlist (Under development)
+              </Alert>
+              <Alert
+                className={`${style.alertMenu} ${activeTab === "subscribe" ? style.alertMenuActive : ""}`}
+                onClick={() => blurHandler("subscribe")}
+              >
+                Subscriptions (Under development)
+              </Alert>
+              <Alert
+                className={`${style.alertMenu} ${activeTab === "wallet" ? style.alertMenuActive : ""}`}
+                onClick={() => blurHandler("wallet")}
+              >
+                Wallet (Under development)
+              </Alert>
+              <Alert
+                className={`${style.alertMenu} ${activeTab === "orders" ? style.alertMenuActive : ""}`}
+                onClick={() => blurHandler("orders")}
+              >
+                My orders (Under development)
+              </Alert>
+            </Nav>
+          </Col>
+          <Col md={9} className={style.contentBlock}>{view}</Col>
+        </Row> */}
+      </Container>
+    </div>
   )
 })
 
