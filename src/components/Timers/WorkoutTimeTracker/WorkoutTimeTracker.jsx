8a515a3f34789280b2d91681e275ebf7d39aee39
l-Spook-l <uaspookua@gmail.com>
2023-11-26 23:16:48 +0200

    Implemented acceptable responsive styles.

--- src/components/Timers/WorkoutTimeTracker/WorkoutTimeTracker.jsx
+++ src/components/Timers/WorkoutTimeTracker/WorkoutTimeTracker.jsx
@@ -1,56 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import style from './WorkoutTimeTracker.module.css'
+import { FaPlay, FaPause } from "react-icons/fa";
+import { GrPowerReset } from "react-icons/gr";
 
 const WorkoutTimeTracker = ({ onStart, onStop, onReset, onTimeUpdate }) => {
-  const [time, setTime] = useState(0);
-  const [isActive, setIsActive] = useState(false);
+  const [time, setTime] = useState(0)
+  const [isActive, setIsActive] = useState(false)
 
   const formatTime = (totalSeconds) => {
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
+    const hours = Math.floor(totalSeconds / 3600)
+    const minutes = Math.floor((totalSeconds % 3600) / 60)
+    const seconds = totalSeconds % 60
 
-    return `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  };
+    return `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+  }
 
   useEffect(() => {
-    let interval;
+    let interval
 
     if (isActive) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 1);
-        onTimeUpdate(time + 1);
-      }, 1000);
+        setTime((prevTime) => prevTime + 1)
+        onTimeUpdate(time + 1)
+      }, 1000)
     } else {
-      clearInterval(interval);
+      clearInterval(interval)
     }
 
-    return () => clearInterval(interval);
-  }, [isActive, time, onTimeUpdate]);
+    return () => clearInterval(interval)
+  }, [isActive, time, onTimeUpdate])
 
   const startTimer = () => {
-    setIsActive(true);
-    onStart();
-  };
+    setIsActive(true)
+    onStart()
+  }
 
   const stopTimer = () => {
-    setIsActive(false);
-    onStop(time);
-  };
+    setIsActive(false)
+    onStop(time)
+  }
 
   const resetTimer = () => {
-    setTime(0);
-    onReset();
-  };
+    setTime(0)
+    onReset()
+  }
 
   return (
     <div className={style.container}>
       <p>Time: {formatTime(time)}</p>
-      <button className={style.buttonTimer} onClick={startTimer}>Start</button>
-      <button className={style.buttonTimer} onClick={stopTimer}>Stop</button>
-      <button className={style.buttonTimer} onClick={resetTimer}>Reset</button>
+      <div className={style.buttonsBlock}>
+        {isActive
+        ? <button className={style.buttonTimer} onClick={stopTimer}><FaPause/></button>
+        : <button className={style.buttonTimer} onClick={startTimer}><FaPlay/></button>
+        }
+        <button disabled={!time > 0} className={style.buttonTimer} onClick={resetTimer}><GrPowerReset/></button>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default WorkoutTimeTracker;
+export default WorkoutTimeTracker
