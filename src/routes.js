b62b3470ee3944de941845ef5113adae4ad6dbff
l-Spook-l <uaspookua@gmail.com>
2023-11-28 17:10:17 +0200

    Added URL for the admin panel.

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
-import {ACTIVE_WORKOUT_ROUTE, CREATE_WORKOUT_ROUTE, MAIN_ROUTE, PROFILE_ROUTE, SHOP_ROUTE, WORKOUTS_ROUTE, WORKOUT_ROUTE, RESET_USER_PASSWORD_ROUTE, GROUP_WORKOUTS_ROUTE, PAGE_404_ROUTE } from './utils/consts'
+import {ACTIVE_WORKOUT_ROUTE, CREATE_WORKOUT_ROUTE, MAIN_ROUTE, PROFILE_ROUTE, SHOP_ROUTE, WORKOUTS_ROUTE, 
+  WORKOUT_ROUTE, RESET_USER_PASSWORD_ROUTE, GROUP_WORKOUTS_ROUTE, PAGE_404_ROUTE, ADMIN_PANEL_ROUTE } from './utils/consts'
 import MainPage from './pages/MainPage/MainPage'
 import WorkoutsPage from './pages/WorkoutsPage/WorkoutsPage'
 import ProfilePage from './pages/Profile/Profile'
@@ -9,6 +10,7 @@ import Shop from './pages/Shop/Shop'
 import ResetUserPassword from './pages/ResetUserPassword/ResetUserPassword'
 import GroupWorkouts from './pages/GroupWorkouts/GroupWorkouts'
 import Page404 from './pages/Page404/Page404'
+import AdminPanelPage from './pages/AdminPanelPage/AdminPanelPage'
 
 export const authRoutes = [
   {
@@ -22,6 +24,10 @@ export const authRoutes = [
   {
     path: ACTIVE_WORKOUT_ROUTE + "/:workout_id",
     Component: ActiveWorkoutPage,
+  },
+  {
+    path: ADMIN_PANEL_ROUTE,
+    Component: AdminPanelPage,
   },
 ]
 
