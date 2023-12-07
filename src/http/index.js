82e6e6d74b54776eb24335c9ba4806af43d480da
l-Spook-l <uaspookua@gmail.com>
2023-12-07 20:47:04 +0200

    add all

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//console.log(process.env.REACT_APP_API_URL)
+// console.log(process.env.REACT_APP_API_URL)
 const $host = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
